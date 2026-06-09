b205768583315a23e17c2d6d48770eba1ef4d513
sihem Hadj-tahar <60602079+sihemht@users.noreply.github.com>
2026-06-09 16:47:45 +0200

    feat: products api & remove user files

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -3,52 +3,77 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Product;
+use App\Services\ApiResponseServices;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //1 endpoint permettant la récupération de la liste des produits (ajouter la capacité de filtrer sur une catégorie)
+    public function __construct(private readonly ApiResponseServices $apiResponseServices)
+    {
+
+    }
+    // List & Filter
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        $query = Product::with('category');
+
+        if($request->has('category_id')) {
+            $query->where('category_id', $request->get('category_id'));
+        }
+        return $this->apiResponseServices->success($query->get(), 'Product list successfully');
     }
 
-    //1 endpoint permettant la création d’un produit
+    //New product
     /**
      * Store a newly created resource in storage.
      */
     public function store(Request $request)
     {
-        //
+        $validateData= $request->validate([
+            'name' => 'required|string|max:255',
+            'price' => 'required|numeric|min:0',
+            'image' => 'string|nullable',
+            'category_id' => 'required|exists:categories,id',
+        ]);
+
+        $product = Product::create($validateData);
+        return $this->apiResponseServices->success($product, 'Product created successfully', 201);
     }
 
-    //1 endpoint permettant la récupération d’un produit
+    //Product $product do findOrFail
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Product $product)
     {
-        //
+        return $this->apiResponseServices->success($product, 'Product find successfully');
     }
 
-    //1 endpoint permettant l’édition d’un produit
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request,Product $product)
     {
-        //
+        $validateData= $request->validate([
+            'name' => 'sometimes|required|string|max:255',
+            'price' => 'sometimes|required|numeric|min:0',
+            'image' => 'string|nullable',
+            'category_id' => 'sometimes|required|exists:categories,id',
+        ]);
+        $product->update($validateData);
+        return $this->apiResponseServices->success($product, 'Product updated successfully');
     }
 
-    //1 endpoint permettant la suppression d’un produit
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Product $product)
     {
-        //
+        $product->delete();
+        return $this->apiResponseServices->success($product, 'Product deleted successfully');
     }
 }
---
+++ app/Services/ApiResponseServices.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Services;
+
+
+use Illuminate\Http\JsonResponse;
+
+class ApiResponseServices {
+
+    public function success(mixed $data, string $message = 'Success',int $statusCode = 200):JsonResponse
+    {
+        return response()->json([
+            'success' => true,
+            'message' => $message,
+            'data' => $data
+        ], $statusCode);
+
+    }
+    public function failure(string $message = 'Failure', array $errors = [], int $statusCode = 400):JsonResponse
+    {
+        $response = [
+            'success' => false,
+            'message' => $message,
+        ];
+
+        if(!empty($errors)) {
+            $response['errors'] = $errors;
+        }
+
+        return response()->json($response, $statusCode);
+    }
+}
