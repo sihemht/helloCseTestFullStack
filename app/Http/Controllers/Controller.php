87606de8054f83a5c258b420ebf0a2c87ce59d6b
sihem Hadj-tahar <60602079+sihemht@users.noreply.github.com>
2026-06-09 16:47:45 +0200

    feat: category api
    feat: product & category api

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
-abstract class Controller
+use App\Http\Controllers\Api\ProductController;
+
+class Controller
 {
-    //
+
 }
--- routes/api.php
+++ routes/api.php
@@ -2,11 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\ProductController;
+use App\Http\Controllers\Api\CategoryController;
 
-/**Route::get('/user', function (Request $request) {
- *    return $request->user();
- *})->middleware('auth:sanctum');
- */
 
-Route::apiResource('products', 'ProductController');
-Route::apiResource('categories', 'CategoryController');
+Route::apiResource('products', ProductController::class);
+Route::apiResource('categories', CategoryController::class);
